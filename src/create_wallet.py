3ba9c08b4859bd40f81e74c833191f27dbeeefff
loveyousomuch <loveyousomuch554@gmail.com>
2021-11-29 13:00:35 +0300

    generate qr code

--- src/create_wallet.py
+++ src/create_wallet.py
@@ -3,6 +3,7 @@
 from secrets import token_bytes
 from coincurve import PublicKey
 from sha3 import keccak_256
+import qrcode
 
 
 def get_pair():
@@ -14,9 +15,14 @@ def get_pair():
 def get_addr(public_key):
     return keccak_256(public_key).digest()[-20:].hex()
 
+def create_qr(name, addr):
+    img = qrcode.make(addr)
+    img.save(name + '.png')
+
 def run():
     private_key, public_key = get_pair()
     addr = get_addr(public_key)
+    create_qr('qr', '0x' + addr)
     print('Addr: 0x' + addr)
     print('Private key:', private_key.hex())
     print('Public key :', public_key.hex())
