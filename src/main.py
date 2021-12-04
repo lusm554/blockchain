513cfcf6a9cf305f33bbc958f018d757c8b141bb
loveyousomuch <loveyousomuch554@gmail.com>
2021-12-04 17:24:41 +0300

    python mongo api works too

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print('how blockchain work??')
+from pymongo import MongoClient
+
+host, port = ('localhost', 27017)
+client = MongoClient(host, port)
+
+
+db = client.test_database
+collection = db.test_collection
+
+test_id = collection.insert_one({"test": 123}).inserted_id
+
+print(test_id)
+
+test = collection.find_one()
+
+print(test)
